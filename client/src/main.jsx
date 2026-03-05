241caa6ff0ae8b445069ab67159371c9328a8f47
steller520 <shivamnegi571@gmail.com>
2026-03-05 23:49:29 +0530

    feat: integrate Tailwind CSS and update project dependencies

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/dashboard",
+        element: null
+      }
+    ]
+  },
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    < RouterProvider router={router} />
+  
 )
