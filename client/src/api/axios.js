284c9cb221ac9f945547f3bc74f12db17b386e5d
steller520 <shivamnegi571@gmail.com>
2026-03-21 13:06:35 +0530

    fix: revert baseURL in axios configuration to use environment variable

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://document-signature-app-hx9h.onrender.com",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
 });
 
 API.interceptors.request.use((config) => {
